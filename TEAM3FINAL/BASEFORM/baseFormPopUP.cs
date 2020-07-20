25b54cf943a40cdd4bfddfd5d43531717573c71c
zlf2603@naver.com <zlf2603@naver.com>
2020-07-20 18:14:53 +0900

    팝업베이스폼등록

---
+++ TEAM3FINAL/BASEFORM/baseFormPopUP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TEAM3FINAL.BaseForm
+{
+    public partial class baseFormPopUP : TEAM3FINAL.ProjectBaseForm
+    {
+        public baseFormPopUP()
+        {
+            InitializeComponent();
+
+            //메인 패널 위치 및 크기 지정
+            panel1.Location = new Point(5, 5);
+            panel1.Size = new Size(this.Width-10, 45);
+
+            button1.Text = "버튼";
+
+
+        }
+    }
+}
