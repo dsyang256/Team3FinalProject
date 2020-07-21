bddbcc6c689b854b439527561346492b0117d094
zlf2603@naver.com <zlf2603@naver.com>
2020-07-21 17:49:18 +0900

    managerVO추가

---
+++ TEAM3FINALVO/MANAGER_VO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEAM3FINALVO
+{
+   public class MANAGER_VO
+    {
+         public string   MANAGER_ID             {get;set;}
+         public string   MANAGER_NAME           {get;set;}
+         public string   MANAGER_EML            {get;set;}
+         public string   MANAGER_PSWD           {get;set;}
+         public string MANAGER_DEP { get; set; }
+    }
+}
