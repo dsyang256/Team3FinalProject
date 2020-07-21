1ee6a822d78bba67972b441651fd46214f0a4c2c
황현우 <melody0405i@gmail.com>
2020-07-21 17:04:24 +0900

    현우

--- TEAM3FINAL/BaseForm/baseForm.Designer.cs
+++ TEAM3FINAL/BaseForm/baseForm.Designer.cs
@@ -126,7 +126,7 @@
         public System.Windows.Forms.Panel panel2;
         public System.Windows.Forms.Label label1;
         public System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
+        public System.Windows.Forms.Panel panel3;
+        public System.Windows.Forms.Panel panel4;
     }
 }
