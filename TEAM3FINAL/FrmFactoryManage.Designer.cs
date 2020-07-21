3f425e74ed2ad1c6935a7187c9066af6147c8859
황현우 <melody0405i@gmail.com>
2020-07-21 18:19:15 +0900

    현우

--- TEAM3FINAL/FrmFactoryManage.Designer.cs
+++ TEAM3FINAL/FrmFactoryManage.Designer.cs
@@ -28,14 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtFactoryCode = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.dgvFactoryList = new WindowsFormsApp18.MyDataGridView();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFactoryList)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -45,9 +52,9 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel2.Controls.Add(this.comboBox5);
+            this.panel2.Controls.Add(this.cboCategory);
             this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.txtFactoryCode);
             this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.label11);
             this.panel2.Controls.Add(this.label3);
@@ -56,13 +63,32 @@
             // label1
             // 
             this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.Text = "공장정의";
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(146, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(193, 23);
-            this.textBox1.TabIndex = 21;
+            this.label2.Location = new System.Drawing.Point(981, 111);
+            this.label2.Visible = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dgvFactoryList);
+            this.panel3.Location = new System.Drawing.Point(15, 141);
+            this.panel3.Size = new System.Drawing.Size(1053, 371);
+            this.panel3.Controls.SetChildIndex(this.panel4, 0);
+            this.panel3.Controls.SetChildIndex(this.dgvFactoryList, 0);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(0, 392);
+            // 
+            // txtFactoryCode
+            // 
+            this.txtFactoryCode.Location = new System.Drawing.Point(146, 19);
+            this.txtFactoryCode.Name = "txtFactoryCode";
+            this.txtFactoryCode.Size = new System.Drawing.Size(193, 23);
+            this.txtFactoryCode.TabIndex = 21;
             // 
             // label11
             // 
@@ -89,7 +115,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.label4.Location = new System.Drawing.Point(556, 22);
+            this.label4.Location = new System.Drawing.Point(475, 21);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(17, 17);
             this.label4.TabIndex = 20;
@@ -98,28 +124,71 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(580, 24);
+            this.label5.Location = new System.Drawing.Point(499, 23);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(43, 15);
             this.label5.TabIndex = 19;
             this.label5.Text = "시설군";
             // 
-            // comboBox5
+            // cboCategory
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(665, 21);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(193, 23);
-            this.comboBox5.TabIndex = 25;
+            this.cboCategory.FormattingEnabled = true;
+            this.cboCategory.Location = new System.Drawing.Point(584, 20);
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Size = new System.Drawing.Size(193, 23);
+            this.cboCategory.TabIndex = 25;
+            // 
+            // dgvFactoryList
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvFactoryList.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvFactoryList.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvFactoryList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvFactoryList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvFactoryList.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvFactoryList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvFactoryList.EnableHeadersVisualStyles = false;
+            this.dgvFactoryList.Location = new System.Drawing.Point(0, 0);
+            this.dgvFactoryList.Name = "dgvFactoryList";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvFactoryList.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvFactoryList.RowHeadersWidth = 30;
+            this.dgvFactoryList.RowTemplate.Height = 150;
+            this.dgvFactoryList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFactoryList.Size = new System.Drawing.Size(1053, 371);
+            this.dgvFactoryList.TabIndex = 8;
             // 
             // FrmFactoryManage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(1080, 565);
             this.Name = "FrmFactoryManage";
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFactoryList)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,11 +196,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFactoryCode;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox cboCategory;
+        private WindowsFormsApp18.MyDataGridView dgvFactoryList;
     }
 }
--- TEAM3FINAL/FrmFactoryManage.cs
+++ TEAM3FINAL/FrmFactoryManage.cs
@@ -14,5 +14,29 @@ namespace TEAM3FINAL
         {
             InitializeComponent();
         }
+
+        private void DataGridViewColumnSet()
+        {
+            //데이터그리드뷰 초기설정
+            Util.InitSettingGridView(dgvFactoryList);
+            ////데이터그리드뷰 체크박스, 컬럼 추가
+            Util.DataGridViewCheckBoxSet(dgvFactoryList, "");
+            
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "시설군", "FAC_FCLTY", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "시설구분", "FAC_TYP", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "시설코드", "FAC_CODE", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "시설명", "FAC_NAME", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "상위시설", "FAC_FCLTY_PARENT", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "시설설명", "FAC_DESC", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "유무상구분", "FAC_FREE_YN", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "시설구분순서", "FAC_TYP_SORT", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "수요차감", "FAC_DEMAND_YN", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "공정차감", "FAC_PROCS_YN", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "자재차감여부", "FAC_MTRL_YN", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "업체코드", "COM_CODE", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "사용유무", "FAC_USE_YN", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "최종수정자", "FAC_LAST_MDFR", true, 80);
+            Util.AddNewColumnToDataGridView(dgvFactoryList, "최종수정시간", "FAC_LAST_MDFY", true, 80);
+        }
     }
 }
