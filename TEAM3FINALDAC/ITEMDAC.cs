706f40c4a4e7bbfdcbd30867e60134301ec44eed
양두성 <entjd256@gmail.com>
2020-07-22 16:21:58 +0900

    양두성

--- TEAM3FINALDAC/ITEMDAC.cs
+++ TEAM3FINALDAC/ITEMDAC.cs
@@ -6,12 +6,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using cryptography;
+using TEAM3FINALVO;
 
 namespace TEAM3FINALDAC
 {
     public class ITEMDAC : ConnectionAccess
     {
-        public DataTable AllITEM()
+        public List<ITEM_VO> AllITEM()
+        {
+            List<ITEM_VO> list = default;
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    AESSalt salt = new AESSalt();
+                    cmd.Connection = new SqlConnection(salt.Decrypt(this.ConnectionString));
+                    cmd.CommandText = @"select ROW_NUMBER() OVER(ORDER BY(SELECT 1)) idx,ITEM_TYP,ITEM_CODE,ITEM_NAME,ITEM_STND,ITEM_UNIT,ITEM_QTY_UNIT,ITEM_UNIT_CNVR,ITEM_QTY_CNVR,ITEM_INCOME_YN,ITEM_PROCS_YN,ITEM_XPORT_YN,ITEM_DSCN_YN,ITEM_FREE_YN,ITEM_COM_DLVR,ITEM_COM_REORDER
+                                                       from ITEM";
+                    cmd.Connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    list = Helper.DataReaderMapToList<ITEM_VO>(reader);
+                    cmd.Connection.Close();
+                    return list;
+                }
+            }
+            catch (Exception err)
+            {
+                return list;
+            }
+            
+        }
+        public DataTable GetSearchItem()
         {
             DataTable dt = new DataTable();
             try
@@ -24,7 +49,7 @@ namespace TEAM3FINALDAC
                     string sql = @"select ROW_NUMBER() OVER(ORDER BY(SELECT 1)) idx,ITEM_TYP,ITEM_CODE,ITEM_NAME,ITEM_STND,ITEM_UNIT,ITEM_QTY_UNIT,ITEM_UNIT_CNVR,ITEM_QTY_CNVR,ITEM_INCOME_YN,ITEM_PROCS_YN,ITEM_XPORT_YN,ITEM_DSCN_YN,ITEM_FREE_YN,ITEM_COM_DLVR,ITEM_COM_REORDER
                                                        from ITEM;";
                     SqlDataAdapter da = new SqlDataAdapter(sql, conn);
-                    da.Fill(dt);                    
+                    da.Fill(dt);
                 }
                 return dt;
             }
@@ -32,7 +57,7 @@ namespace TEAM3FINALDAC
             {
                 return dt;
             }
-            
+
         }
 
     }
