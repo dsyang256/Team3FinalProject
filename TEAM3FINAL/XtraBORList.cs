30a2c9de37bcfeb066db48b34458448d36eccb18
황현우 <melody0405i@gmail.com>
2020-08-06 14:58:10 +0900

    ddd

---
+++ TEAM3FINAL/XtraBORList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace TEAM3FINAL
+{
+    public partial class XtraBORList : DevExpress.XtraReports.UI.XtraReport
+    {
+        public XtraBORList()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
