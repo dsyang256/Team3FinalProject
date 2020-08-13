c4bc56b86e511305bb4715fcd84986ffc2fd9afa
황현우 <melody0405i@gmail.com>
2020-08-13 18:23:15 +0900

    현우

--- TEAM3FINALDAC/ProductOUTDAC.cs
+++ TEAM3FINALDAC/ProductOUTDAC.cs
@@ -18,17 +18,68 @@ namespace TEAM3FINALDAC
             {
                 cmd.Connection = new SqlConnection(this.ConnectionString);
                 cmd.CommandText = @"select w.SALES_WORK_ORDER_ID, s.COM_CODE, c.COM_NAME, s.SALES_COM_CODE, w.ITEM_CODE, i.ITEM_NAME, convert(nvarchar, s.SALES_DUEDATE, 23) SALES_DUEDATE, w.WO_PLAN_QTY, 
-	                                       sum(case when INS_TYP = '출고' then (INS_QTY*-1) when INS_TYP = '입고' then INS_QTY end) INS_QTY
+	                                       sum(case when INS_TYP = '출고' then (INS_QTY*-1) when INS_TYP = '입고' then INS_QTY end) INS_QTY,
+	                                       (select isnull(sum(case when INS_TYP = '출고' then INS_QTY end), 0)
+		                                    from INSTACK 
+                                            where SALES_WORK_ORDER_ID = w.SALES_WORK_ORDER_ID and INS_WRHS = ii.INS_WRHS) as OUTed_QTY, (0) OUTing_QTY
                                     from WORKORDER w inner join SALES_WORK s on w.SALES_WORK_ORDER_ID = s.SALES_Work_Order_ID
 				                                     inner join COMPANY c on s.COM_CODE = c.COM_CODE
 				                                     inner join ITEM i on w.ITEM_CODE = i.ITEM_CODE
 				                                     inner join INSTACK ii on w.ITEM_CODE = ii.ITEM_CODE
-                                    where INS_WRHS = 'M_01'
-                                    group by w.SALES_WORK_ORDER_ID, s.COM_CODE, c.COM_NAME, s.SALES_COM_CODE, w.ITEM_CODE, i.ITEM_NAME, s.SALES_DUEDATE, w.WO_PLAN_QTY";
+                                    where INS_WRHS = 'M_01' and w.WO_WORK_STATE <> '마감완료'
+                                    group by w.SALES_WORK_ORDER_ID, s.COM_CODE, c.COM_NAME, s.SALES_COM_CODE, w.ITEM_CODE, i.ITEM_NAME, s.SALES_DUEDATE, w.WO_PLAN_QTY, ii.INS_WRHS";
                 cmd.Connection.Open();
                 SqlDataReader reader = cmd.ExecuteReader();
                 list = Helper.DataReaderMapToList<ProductOUT_VO>(reader);
                 return list;
+            }
+        }
+
+        public Message ProductOUT(ProductOUT_VO vo, string id)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = new SqlConnection(this.ConnectionString);
+                    cmd.CommandText = "SP_ProductOUT";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@P_SALES_WORK_ORDER_ID", vo.SALES_WORK_ORDER_ID);
+                    cmd.Parameters.AddWithValue("@P_SALES_COM_CODE", vo.SALES_COM_CODE);
+                    cmd.Parameters.AddWithValue("@P_ITEM_CODE", vo.ITEM_CODE);
+                    cmd.Parameters.AddWithValue("@P_WO_PLAN_QTY", vo.WO_PLAN_QTY);
+                    cmd.Parameters.AddWithValue("@P_INS_QTY", vo.INS_QTY);
+                    cmd.Parameters.AddWithValue("@P_OUTed_QTY", vo.OUTed_QTY);
+                    cmd.Parameters.AddWithValue("@P_OUTing_QTY", vo.OUTing_QTY);
+                    cmd.Parameters.AddWithValue("@P_WO_LAST_MDFR", id);
+                    cmd.Parameters.Add(new SqlParameter("@P_ReturnCode", System.Data.SqlDbType.NVarChar, 5));
+                    cmd.Parameters["@P_ReturnCode"].Direction = System.Data.ParameterDirection.Output;
+
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+
+                    string result = cmd.Parameters["@P_ReturnCode"].Value.ToString();
+
+                    Message message = new Message();
+                    if (result == "S01")
+                    {
+                        message.IsSuccess = true;
+                        message.ResultMessage = "성공적으로 등록되었습니다.";
+                    }                 
+                    else if (result == "S00")
+                    {
+                        message.IsSuccess = false;
+                        message.ResultMessage = "실패하였습니다.";
+                    }
+
+                    return message;
+                }
+
+            }
+            catch(Exception err)
+            {
+                return new Message(err);
             }
         }
     }
