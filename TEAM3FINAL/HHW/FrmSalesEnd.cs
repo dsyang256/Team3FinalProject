58dbf84956d94bae0cf427ecb041d84746d48348
melody0405@daum.net <melody0405@daum.net>
2020-08-16 18:24:34 +0900

    현우

--- TEAM3FINAL/HHW/FrmSalesEnd.cs
+++ TEAM3FINAL/HHW/FrmSalesEnd.cs
@@ -5,14 +5,143 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using TEAM3FINAL.Services;
+using TEAM3FINALVO;
+using System.Linq;
+using Message = TEAM3FINALVO.Message;
+using TEAM3FINAL;
 
 namespace TEAM3FINAL
 {
-    public partial class FrmSalesEnd : TEAM3FINAL.baseForm
+    public partial class FrmSalesEnd : TEAM3FINAL.baseForm, CommonBtn
     {
+        CheckBox headerChk;
+
         public FrmSalesEnd()
         {
             InitializeComponent();
         }
+
+        #region 체크박스 포함한 그리드뷰 컬럼 생성
+
+        private void DataGridViewColumnSet()
+        {
+            //데이터그리드뷰 초기설정
+            DataGridViewUtil.InitSettingGridView(dgvSalesEnd);
+            //데이터그리드뷰 체크박스 컬럼 추가
+            DataGridViewUtil.DataGridViewCheckBoxSet(dgvSalesEnd, "");
+            //일반컬럼 추가
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "고객주문번호", "SALES_WORK_ORDER_ID", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "고객사", "COM_NAME", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "도착지명", "SALES_COM_CODE", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "품목", "ITEM_CODE", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "품명", "ITEM_NAME", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "납기일", "SALES_DUEDATE", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "주문수량", "WO_PLAN_QTY", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "출하수량", "OUTed_QTY", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "마감수량", "WO_PLAN_QTY", true, 80);
+            DataGridViewUtil.AddNewColumnToDataGridView(dgvSalesEnd, "매출확정금액", "EndPrice", true, 80);
+            DataGridViewUtil.DataGridViewRowNumSet(dgvSalesEnd);
+            DataGridViewCheckBoxAllCheck();
+        }
+
+        private void DataGridViewCheckBoxAllCheck()
+        {
+            headerChk = new CheckBox();
+            Point headerCell = dgvSalesEnd.GetCellDisplayRectangle(0, -1, true).Location;
+            headerChk.Location = new Point(headerCell.X + 4, headerCell.Y + 15);
+            headerChk.Size = new Size(18, 18);
+            headerChk.BackColor = Color.FromArgb(245, 245, 246);
+            headerChk.Click += HeaderChk_Clicked;
+            dgvSalesEnd.Controls.Add(headerChk);
+        }
+
+        private void HeaderChk_Clicked(object sender, EventArgs e)
+        {
+            dgvSalesEnd.EndEdit();
+
+            //데이터그리드뷰의 전체 행의 체크를 체크 or 언체크
+            foreach (DataGridViewRow row in dgvSalesEnd.Rows)
+            {
+                DataGridViewCheckBoxCell chk = (DataGridViewCheckBoxCell)row.Cells[0];
+                chk.Value = headerChk.Checked;
+            }
+        }
+
+        #endregion
+
+
+        private void btnset()
+        {
+            FrmMAIN frm = (FrmMAIN)this.MdiParent;
+            frm.eSearch += Search;
+            frm.eInsert += Insert;
+            frm.eDelete += Delete;
+            frm.eUpdate += Update;
+            frm.eReset += Reset;
+            frm.ePrint += Print;
+        }
+
+        private void FrmSalesEnd_Load(object sender, EventArgs e)
+        {
+            DataGridViewColumnSet();
+            btnset();
+            GetSalesEnd();
+        }
+
+        private void GetSalesEnd()
+        {
+            SalesEndService service = new SalesEndService();
+            dgvSalesEnd.DataSource = null;
+            dgvSalesEnd.DataSource = service.GetSalesEnd();
+        }
+
+        private void FrmSalesEnd_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FrmMAIN frm = (FrmMAIN)this.MdiParent;
+            frm.eSearch -= Search;
+            frm.eInsert -= Insert;
+            frm.eDelete -= Delete;
+            frm.eUpdate -= Update;
+            frm.eReset -= Reset;
+            frm.ePrint -= Print;
+        }
+
+
+        #region ****메인 버튼 이벤트****
+
+        public void Insert(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Search(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        
     }
 }
---
+++ TEAM3FINALDAC/SalesEndDAC.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEAM3FINALVO;
+
+namespace TEAM3FINALDAC
+{
+    public class SalesEndDAC : ConnectionAccess
+    {
+        public List<SalesEnd_VO> GetSalesEnd()
+        {
+            List<SalesEnd_VO> list = null;
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(this.ConnectionString);
+                cmd.CommandText = @"select w.SALES_WORK_ORDER_ID, s.COM_CODE, c.COM_NAME, s.SALES_COM_CODE, w.ITEM_CODE, i.ITEM_NAME, convert(nvarchar, s.SALES_DUEDATE, 23) SALES_DUEDATE, w.WO_PLAN_QTY, 
+	   (select isnull(sum(case when INS_TYP = '출고' then INS_QTY end), 0)
+		from INSTACK 
+		where SALES_WORK_ORDER_ID = w.SALES_WORK_ORDER_ID and INS_WRHS = ii.INS_WRHS) as OUTed_QTY, w.WO_PLAN_QTY,
+	   (select isnull(sum(case when INS_TYP = '출고' then INS_QTY end), 0)
+		from INSTACK 
+		where SALES_WORK_ORDER_ID = w.SALES_WORK_ORDER_ID and INS_WRHS = ii.INS_WRHS) * 
+	   (select top 1 MC_UNITPRICE_CUR 
+		from MATERIAL_COST
+		where ITEM_Code = w.ITEM_CODE
+		order by MC_ENDDATE desc) as EndPrice
+from WORKORDER w inner join SALES_WORK s on w.SALES_WORK_ORDER_ID = s.SALES_Work_Order_ID
+				 inner join COMPANY c on s.COM_CODE = c.COM_CODE
+				 inner join ITEM i on w.ITEM_CODE = i.ITEM_CODE
+				 inner join INSTACK ii on w.ITEM_CODE = ii.ITEM_CODE
+				 inner join MATERIAL_COST m on w.ITEM_CODE = m.ITEM_Code
+where ii.INS_WRHS = 'M_01' and (w.WO_WORK_STATE = '마감완료' or w.WO_WORK_STATE = '마감중') and ii.INS_TYP = '출고'
+group by w.SALES_WORK_ORDER_ID, s.COM_CODE, c.COM_NAME, s.SALES_COM_CODE, w.ITEM_CODE, i.ITEM_NAME, s.SALES_DUEDATE, w.WO_PLAN_QTY, ii.INS_WRHS
+order by SALES_DUEDATE";
+                cmd.Connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                list = Helper.DataReaderMapToList<SalesEnd_VO>(reader);
+                return list;
+            }
+        }
+    }
+}
