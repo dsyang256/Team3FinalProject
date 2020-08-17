62a7774a10ac95cc5c6f7e9128278d722560aa45
zlf2603@naver.com <zlf2603@naver.com>
2020-08-17 12:06:18 +0900

    로그 수정

--- TEAM3FINAL/Program.cs
+++ TEAM3FINAL/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,15 +9,31 @@ namespace TEAM3FINAL
 {
     static class Program
     {
+        // 로거 ILog 필드
+        private static ILog log = LogManager.GetLogger("Program");
+
         /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {            
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Application.ThreadException += Application_ThreadException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMAIN());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            log.Error(e.Exception.Message);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            //로그파일에 처리되지 않은 예외를 받아서 처리.
+            log.Error(e.ExceptionObject.ToString());
         }
     }
 }
