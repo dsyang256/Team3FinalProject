d9a5d48d4dd0a98686089976f0040770fed4500e
zlf2603@naver.com <zlf2603@naver.com>
2020-08-18 18:33:46 +0900

    수정

---
+++ TEAM3FINALWEB/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TEAM3FINALWEB.DAC;
+
+namespace TEAM3FINALWEB.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            OrderDAC dac = new OrderDAC();
+            DataTable dt = dac.GetOrderSalesByMonth();
+
+            string data1 = default; //제품1 y축 데이터
+            List<string> prodNM = new List<string>(); // 제품명
+
+            string labels = "1월,2월,3월,4월,5월,6월,7월,8월,9월,10월,11월,12월";
+
+            ViewBag.Labels = labels;
+            DataView dv = new DataView(dt);
+            ViewBag.Label1 = dv.ToTable().Rows[0][12];
+            ViewBag.data1 = data1;
+
+            List<int> qtys = new List<int>();
+
+            for (int i = 0; i < dv.ToTable().Columns.Count-1; i++)
+            {
+                data1 = "[" + string.Join(",", Convert.ToInt32(dv.ToTable().Rows[0][i])) + "]";
+            }
+
+            string a = data1;
+
+            return View();
+        }
+    }
+}
---
+++ TEAM3FINALWEB/DAC/OrderDAC.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient; //add
+using System.Web.Configuration; //add
+using TEAM3FINALWEB.Models;
+using System.Security.AccessControl;
+using System.Web.UI.WebControls;
+using System.Drawing.Printing;
+using System.Data;
+
+namespace TEAM3FINALWEB.DAC
+{
+    public class OrderDAC : ConnectionAccess
+    {
+        string connStr = string.Empty;
+        public OrderDAC()
+        {
+            connStr = this.ConnectionString;
+        }
+
+        public System.Data.DataTable GetOrderSalesByMonth()
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string sql = $@"
+                        -- 제품별 월별 매출액
+                        select 
+                        isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 1 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M01
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 2 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M02				
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 3 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M03					
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 4 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M04			
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 5 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M05			
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 6 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M06			
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 7 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M07			
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 8 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M08			
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 9 THEN SUM(SALES_QTY*SALES_TTL) END,0) AS M09			
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 10 THEN SUM( SALES_QTY*SALES_TTL) END,0) AS M10			
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 11 THEN SUM( SALES_QTY*SALES_TTL) END,0) AS M11			
+		                        ,isnull( case Month(convert(nvarchar(10),SALES_ENDDATE,23)) when 12 THEN SUM( SALES_QTY*SALES_TTL) END,0) AS M12
+                        ,ITEM_CODE
+                        from SALES_RECORD
+                        where 1=1
+                        AND YEAR(CONVERT(nvarchar(10),SALES_ENDDATE,23)) = '2020'
+                        group by Month(convert(nvarchar(10),SALES_ENDDATE,23)),ITEM_CODE
+                        order by Month(convert(nvarchar(10),SALES_ENDDATE,23)),ITEM_CODE
+                        ";
+
+                SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+    }
+}
